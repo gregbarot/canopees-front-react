fdf2c7fd5b30f60b6d2fdaf2363e985c52d726dd
Gregory Barot <gregbarot@gmail.com>
2026-07-07 09:35:35 +0200

    modification d'un className dans DEscription .jsx

--- src/components/Accueil/Description.jsx
+++ src/components/Accueil/Description.jsx
@@ -7,7 +7,7 @@ export default function Description({ content }) {
     <>
       <section className="pt-4" id="description">
         <h1>{content.title}</h1>
-        <div class="rich-text"
+        <div className="rich-text"
         dangerouslySetInnerHTML={{ __html: content.textContent }}
         />
         
